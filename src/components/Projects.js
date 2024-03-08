a9ed433c99910b48d6432826941c315d916d4d34
LB45000 <144392042+LB45000@users.noreply.github.com>
2024-03-07 17:27:06 -0800

    Add files via upload

---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div className="projects-section">
+      <h1>My Projects</h1>
+      <p>Here are some of the projects I've worked on. Click the link below to view my GitHub profile and explore my repositories.</p>
+      <a href="https://github.com/LB45000" target="_blank" rel="noopener noreferrer">View My GitHub</a>
+    </div>
+  );
+};
+
+export default Projects;
